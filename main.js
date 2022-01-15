a3c32a5240aa79effd85e18a3ca4f61e911eb949
aanyaagrawal0205 <80906217+aanyaagrawal0205@users.noreply.github.com>
2022-01-15 11:02:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function switchingbed() {
+    window.location="bedroom.html"
+}
+function switchingfruits() {
+    window.location="fruits.html"
+}
+function switchingTV() {
+    window.location="TV.html"
+}
+function switchingbottles() {
+    window.location="Bottles.html"
+}
+function switchingdesk() {
+    window.location="desk.html"
+}
